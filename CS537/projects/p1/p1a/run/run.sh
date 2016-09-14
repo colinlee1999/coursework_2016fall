56da2af50d95763e0f53d513edf62e7239e95208
colinlee1999 <yfl@emperor-04.cs.wisc.edu>
2016-09-14 13:14:52 -0500

    sort finished, need test and polish

--- CS537/projects/p1/p1a/run/run.sh
+++ CS537/projects/p1/p1a/run/run.sh
@@ -1,1 +1,2 @@
-./bin/genvar.out -s 0 -n 100 -m 32 -o data/input_file
+source ./run/sort.sh
+source ./run/dump_output.sh
--- CS537/projects/p1/p1a/src/varsort.c
+++ CS537/projects/p1/p1a/src/varsort.c
@@ -1,3 +1,5 @@
+//TODO: add error message if malloc fails
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,23 +9,41 @@
 #include <string.h>
 #include "sort.h"
 
-void usage(char *prog)
+void
+usage(char *prog)
 {
 	fprintf(stderr, "usage: %s <-i input file> <-o output file>\n", prog);
 	exit(1);
 }
 
-int main(int argc, char *argv[])
+static int
+cmp_rec_dataptr_t(const void * p1, const void * p2)
+{
+  rec_dataptr_t * r1 = (rec_dataptr_t *)p1;
+  rec_dataptr_t * r2 = (rec_dataptr_t *)p2;
+  if (r1->key < r2->key)
+    return -1;
+  if (r1->key > r2->key)
+    return 1;
+  return 0;
+}
+
+int
+main(int argc, char *argv[])
 {
   // arguments
-  char *inFile = "/no/such/file";
+  char * inFile = "/no/such/file";
+  char * outFile = "/no/such/file";
   int c;
 
   opterr = 0;
-  while ((c = getopt(argc, argv, "i:")) != -1) {
+  while ((c = getopt(argc, argv, "i:o:")) != -1) {
     switch (c) {
     case 'i':
       inFile = strdup(optarg);
+      break;
+    case 'o':
+      outFile = strdup(optarg);
       break;
     default:
       usage(argv[0]);
@@ -37,6 +57,13 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
+  // open and create output file
+  int out_file = open(outFile, O_WRONLY|O_CREAT|O_TRUNC, S_IRWXU);
+  if (out_file < 0) {
+    perror("open");
+    exit(1);
+  }
+
   // output the number of keys as a header for this file
   int recordsLeft;
   int rc;
@@ -46,39 +73,62 @@ int main(int argc, char *argv[])
     perror("read");
     exit(1);
   }
+
+  rc = write(out_file, &recordsLeft, sizeof(recordsLeft));
+  if (rc != sizeof(recordsLeft))
+  {
+    perror("write");
+    exit(1);
+  }
+
+  rec_dataptr_t * data_head;
+  data_head = (rec_dataptr_t *)malloc(recordsLeft * sizeof(rec_dataptr_t));
   int count = 0;
-  printf("Number of records: %d\n", recordsLeft);
 
-  rec_nodata_t r;
-  unsigned int data[MAX_DATA_INTS];
+  while (recordsLeft--) {
 
-  while (recordsLeft) {	
-
+    
     // Read the fixed-sized portion of record: key and size of data
-    rc = read(fd, &r, sizeof(rec_nodata_t));
+    rc = read(fd, data_head + count, sizeof(rec_nodata_t));
     if (rc != sizeof(rec_nodata_t)) {
       perror("read");
       exit(1);
     }
-    assert(r.data_ints <= MAX_DATA_INTS);
+
+    int num_data_ints = data_head[count].data_ints;
+
+    assert(num_data_ints <= MAX_DATA_INTS);
+
+    data_head[count].data_ptr = (unsigned int *)malloc(num_data_ints * sizeof(unsigned int));
 
     // Read the variable portion of the record
-    rc = read(fd, &data, r.data_ints * sizeof(unsigned int));
-    if (rc !=  r.data_ints * sizeof(unsigned int)) {
+    rc = read(fd, data_head[count].data_ptr, num_data_ints * sizeof(unsigned int));
+    if (rc !=  num_data_ints * sizeof(unsigned int)) {
       perror("read");
       exit(1);
     }
 
-    printf("key %d: %u data_ints: %u rec: ", count, r.key, r.data_ints);
-    int j;
-    for (j = 0; j < r.data_ints; j++) 
-      printf("%u ", data[j]);
-    printf("\n");
-		
-    recordsLeft--;
     count++;
+  }
+
+  //sort
+  qsort(data_head, count, sizeof(rec_dataptr_t), cmp_rec_dataptr_t);
+
+  int i;
+  for (i = 0; i < count; i++)
+  {
+    int data_size = 2 * sizeof(unsigned int) + 
+      data_head[i].data_ints * sizeof(unsigned int);
+    
+    rc = write(out_file, data_head + i, data_size);
+    if (rc != data_size)
+    {
+      perror("write");
+      exit(1);
+    }
   }
     
   (void) close(fd);
+  (void) close(out_file);
   return 0;
 }
